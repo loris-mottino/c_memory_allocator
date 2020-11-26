30dace226b62557aef2777222d87362125b1d8aa
verrino <74714549+verrino@users.noreply.github.com>
2020-11-26 11:45:05 +0100

    Update mem.c

--- src/mem.c
+++ src/mem.c
@@ -5,42 +5,129 @@
 
 // Déclaration de la structure de bloc libre
 
-struct fb;
+/* On inclut l'interface publique */
+#include "mem.h"
 
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
 
-
-// Déclaration des méthodes
-
-// CODE
-
-
-
-// Structure de bloc libre (fb_t)
-
-typedef struct fb {
-	size_t taille; // Stocke la taille utilisable du bloc
-	struct fb *prochain; // Pointeur vers le prochain bloc libre
-} bloc_libre;
-
-/* NOTA BENE : Un bloc occupé ne possède pas structure, car son adresse est retenue par l'utilisateur.
- * Néanmoins, il possède une taille stockée 64 bits avant le pointeur vers la mémoire de ce bloc.
+/* Définition de l'alignement recherché
+ * Avec gcc, on peut utiliser __BIGGEST_ALIGNMENT__
+ * sinon, on utilise 16 qui conviendra aux plateformes qu'on cible
  */
+#ifdef __BIGGEST_ALIGNMENT__
+#define ALIGNMENT __BIGGEST_ALIGNMENT__
+#else
+#define ALIGNMENT 16
+#endif
 
+/* structure placée au début de la zone de l'allocateur
 
+   Elle contient toutes les variables globales nécessaires au
+   fonctionnement de l'allocateur
 
-// Déclaration du pointeur qui stocke toute la mémoire allouée.
-void *memoire;
+   Elle peut bien évidemment être complétée
+*/
+struct allocator_header {
+        size_t memory_size;
+	mem_fit_function_t *fit;
+};
 
-
-
-/* 
- * 
- * 
- * 
+/* La seule variable globale autorisée
+ * On trouve à cette adresse le début de la zone à gérer
+ * (et une structure 'struct allocator_header)
  */
+static void* memory_addr;
 
-
-
-int main() {
-  return 0;
+static inline void *get_system_memory_addr() {
+	return memory_addr;
 }
+
+static inline struct allocator_header *get_header() {
+	struct allocator_header *h;
+	h = get_system_memory_addr();
+	return h;
+}
+
+static inline size_t get_system_memory_size() {
+	return get_header()->memory_size;
+}
+
+
+struct fb {
+	size_t size;
+	struct fb* next;
+	/* ... */
+};
+
+
+void mem_init(void* mem, size_t taille)
+{
+        memory_addr = mem;
+        *(size_t*)memory_addr = taille;
+	/* On vérifie qu'on a bien enregistré les infos et qu'on
+	 * sera capable de les récupérer par la suite
+	 */
+	assert(mem == get_system_memory_addr());
+	assert(taille == get_system_memory_size());
+	/* ... */
+	mem_fit(&mem_fit_first);
+}
+
+void mem_show(void (*print)(void *, size_t, int)) {
+	/* ... */
+	while (/* ... */ 0) {
+		/* ... */
+		print(/* ... */NULL, /* ... */0, /* ... */0);
+		/* ... */
+	}
+}
+
+void mem_fit(mem_fit_function_t *f) {
+	get_header()->fit = f;
+}
+
+void *mem_alloc(size_t taille) {
+	/* ... */
+	__attribute__((unused)) /* juste pour que gcc compile ce squelette avec -Werror */
+	struct fb *fb=get_header()->fit(/*...*/NULL, /*...*/0);
+	/* ... */
+	return NULL;
+}
+
+
+void mem_free(void* mem) {
+}
+
+
+struct fb* mem_fit_first(struct fb *list, size_t size) {
+	return NULL;
+}
+
+/* Fonction à faire dans un second temps
+ * - utilisée par realloc() dans malloc_stub.c
+ * - nécessaire pour remplacer l'allocateur de la libc
+ * - donc nécessaire pour 'make test_ls'
+ * Lire malloc_stub.c pour comprendre son utilisation
+ * (ou en discuter avec l'enseignant)
+ */
+size_t mem_get_size(void *zone) {
+	/* zone est une adresse qui a été retournée par mem_alloc() */
+
+	/* la valeur retournée doit être la taille maximale que
+	 * l'utilisateur peut utiliser dans cette zone */
+	return 0;
+}
+
+/* Fonctions facultatives
+ * autres stratégies d'allocation
+ */
+struct fb* mem_fit_best(struct fb *list, size_t size) {
+	return NULL;
+}
+
+struct fb* mem_fit_worst(struct fb *list, size_t size) {
+	return NULL;
+}
+
