14ab859134f0e2300e6a1760ef547450fde7380c
verrino <74714549+verrino@users.noreply.github.com>
2020-11-26 11:32:09 +0100

    Update and rename idees.c to mem.c

---
+++ src/mem.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+// Déclaration de la structure de bloc libre
+
+struct fb;
+
+
+
+// Déclaration des méthodes
+
+// CODE
+
+
+
+// Structure de bloc libre (fb_t)
+
+typedef struct fb {
+	size_t size; // Stocke la taille utilisable du bloc
+	struct fb *next; // Pointeur vers le prochain bloc libre
+} fb_t;
+
+/* NOTA BENE : Un bloc occupé ne possède pas structure, car son adresse est retenue par l'utilisateur.
+ * Néanmoins, il possède une taille stockée 64 bits avant le pointeur vers la mémoire de ce bloc.
+ */
+
+
+
+int main() {
+  return 0;
+}
